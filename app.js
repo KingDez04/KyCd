e538ab828d84def96f9ae932355d36f8c27cdfd0
Ademola Ademeso <desmondademeso@gmail.com>
2024-05-10 17:37:00 +0100

    added dev files

---
+++ app.js
@@ -0,0 +1,19 @@
+const insert = document.getElementById("insert");
+const key = document.querySelector(".key");
+
+window.addEventListener("keydown", e => {
+    insert.innerHTML = `
+        <div class="key">
+            ${e.key === "" ? "space" : e.key}
+            <small>Key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>KeyCode</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>Code</small>
+        </div>
+    `
+});
